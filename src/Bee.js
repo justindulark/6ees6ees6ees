4e1cf4beed3c51f3bf38618a8ae91319bffff577
justin <justindulark@gmail.com>
2017-12-04 11:33:55 -0800

    Add code for class Bee

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,20 @@
-class Bee {
-  // TODO..
+// class Grub {
+//   constructor(age, color, food) {
+//     this.age = 0;
+//     this.color = 'pink';
+//     this.food = 'jelly';
+//   }
+//   eat(){
+//     return 'Mmmmmmmmm jelly';
+//   };
+// };
+
+
+class Bee extends Grub{
+  constructor(age, color, job) {
+    super();
+    this.age = 5;
+    this.color = 'yellow';
+    this.job = 'Keep on growing';
+  }
 };
